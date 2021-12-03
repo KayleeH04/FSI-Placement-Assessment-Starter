95c1ea410c330e88ac02e8750919bba10989306e
KayleeH04 <93634206+KayleeH04@users.noreply.github.com>
2021-12-02 21:38:54 -0800

    completed assigment

--- assets/script.js
+++ assets/script.js
@@ -1,11 +1,11 @@
 // HINT: You can delete this console.log after you no longer need it!
-console.log('JavaScript code has loaded!')
+console.log('click')
 
 // First, tell us your name
 let yourName = 'Kaylee Hernandez' // HINT: Replace this with your own name!
 
 // We'll use these variables to track the counts of each cookie type
-let gb = 0      // Gingerbread
+let gb = 0 
 let cc = 0      // Chocolate Chip
 let sugar = 0   // Sugar Sprinkle
 
@@ -13,11 +13,30 @@ let sugar = 0   // Sugar Sprinkle
 document.getElementById('credit').textContent = `Created by ${yourName}`
 
 // Event listener for clicks on the "+" button for Gingerbread cookies
-document.getElementById('add-gb').addEventListener('click', function() {
+document.getElementById('add-gb').addEventListener('click', function(+=1) {
     // HINT: You can delete this console.log after you no longer need it!
-    console.log('Gingerbread + button was clicked!')
+    quantity+=1;
+    document.getElementById(quantity-number').textContent = quantity;
 
     // TODO: Write the code to be run when the "+" button for "Gingerbread" is clicked
 })
 
 // TODO: Hook up event listeners for the rest of the buttons
+
+let plusButton = document.querySelector.`plus-btn`);
+let minusButton = document.querySelector('.minus-btn');
+
+let quantity = 0;
+
+plusButton.addEventListener(`click`, function() {
+quantity += 1;
+document.getElementById(`quantity-number`).textContent = quantity;
+)}
+
+minusButton.addEventListener(`click`, function() {
+    if (quantity > 0) {
+    quantity  -= 1;
+    }
+    document.getElementById(`quanity-number`).textContent = quantity;
+)}
+
